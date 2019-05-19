39cff7b5f9320adc2064260c7c3b7e1d6deac2c6
Ron Wilson <ron@Rons-MacBook-Pro.local>
2019-05-19 18:46:54 -0500

    First commit.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => <div style={{ color: `purple` }}>
+<h1>Hello there, Gatsby!</h1>
+<p>dadadadada a world.</p>
+<img src="https://source.unsplash.com/random/400x200" alt="" />
+</div>
+
